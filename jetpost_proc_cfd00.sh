d7a25e36e3674a986112762135e4cc37bd6a3f49
skr-code <salmanr96@gmail.com>
2019-08-26 17:05:39 -0500

    first working copy

---
+++ jetpost_proc_cfd00.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo Please enter the directory for input1. Note: it is assumed that all the necessary files have been genereated for input1
+read input1
+
+echo Please enter the directory for input2
+read input2
+
+#Setting up the post-proc
+echo Setting up the post-proc file
+cd $input2
+mkdir $input2/post-proc
+echo Post-proc directory made
+
+#Executing External Rake
+echo Executing external rake
+tec360 -b /home/sal/Desktop/tecplotStuff/macros/M2ExternalDataRakeWithPitot/ExternalRakeWithPitot.mcr
+echo External rake complete
+
+#Executing Internal Rake
+echo Executing internal rake
+tec360 -b /home/sal/Desktop/tecplotStuff/macros/M2InternalDataRakeWithTotal/InternalRakeWithPitot.mcr
+echo Internal rake complete
+
+#Positioning External rake data for matlab script
+echo Positioning external rake data for matlab
+cp $input1/post-proc/external* /home/sal/Desktop/MATLAB/DataRakePlotter/External/input1
+cp $input2/post-proc/external* /home/sal/Desktop/MATLAB/DataRakePlotter/External/input2
+echo External rake data positioned
+
+#Positioning Internal rake data for matlab script
+echo Positioning internal rake data
+cp $input1/post-proc/internal* /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/input1
+cp $input2/post-proc/internal* /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/input2
+echo Internal rake data positioned
---
+++ jetpost_proc_cfd06_copy.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+echo Please enter the directory for input1. Note: it is assumed that all the necessary files have been genereated for input1
+read input1
+
+echo Please enter the directory for input2
+read input2
+
+#External Rake Processing
+echo Starting the external rake data processing
+cd /home/sal/Desktop/MATLAB/DataRakePlotter/External/src
+'/home/sal/softwares/MATLAB/bin/matlab' -nodisplay -nosplash -nodesktop -r 'ExternalRakePlotter;exit;'
+
+chmod u+x /home/sal/Desktop/MATLAB/DataRakePlotter/External/FiguresOutput/*
+echo Moving external rake figures to post-proc directory
+cp -r /home/sal/Desktop/MATLAB/DataRakePlotter/External/FiguresOutput/* $input2/post-proc
+
+echo External rake figures moved to post-proc
+
+#Internal Rake Processing
+echo Starting internal rake data processing
+cd /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/src
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "InternalRakePlotter;exit;"
+
+chmod u+x /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/FiguresOutput/*
+echo Moving internal rake figures to post-proc directory
+cp -r /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/FiguresOutput/* $input2/post-proc
+
+echo Internal rake figures moved to post-proc
+
+#Cleaning Near-Field Data
+#Have to modify to do each monitor point
+chmod u+x $input2/sol/monitor_points_*.dat
+
+echo Moving monitorpoint file to post-proc
+cp -r $input2/sol/monitor_points_*.dat $input2/post-proc
+
+echo Cleaning near-field data
+cp -r $input2/post-proc/monitor_points_*.dat /home/sal/Desktop/MATLAB/MonotonicCleaner/monitor_data
+cd /home/sal/Desktop/MATLAB/MonotonicCleaner
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "extract_mon_pts_pt0;exit;"
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "extract_mon_pts_pt1;exit;"
+
+echo Moving the cleaned monitor point data to post-proc directory
+cp /home/sal/Desktop/MATLAB/MonotonicCleaner/extract_physical_data/* $input2/post-proc/
+
+#Converting Monitor Point Data to SPL
+echo Converting monitor point data to spl
+cd $input2/post-proc
+fftpsd -dft -i cpr-p2-p-pt0.dat -o ./ -l 0.0171083137623 -c 2 -w hann -variance -spl
+fftpsd -dft -i cpr-p2-p-pt1.dat -o ./ -l 0.0171083137623 -c 2 -w hann -variance -spl
+echo Monitor point data conversion complete
+
+#Saving Plots of Near-Field Data
+echo Creating and Storing Near-Field Data Plots
+cp $input1/post-proc/SPL_cpr-p2-p-pt*.dat /home/sal/Desktop/MATLAB/SPLPlotter/MonitorPointPlotter/input1
+cp $input2/post-proc/SPL_cpr-p2-p-pt*.dat /home/sal/Desktop/MATLAB/SPLPlotter/MonitorPointPlotter/input2
+cd /home/sal/Desktop/MATLAB/SPLPlotter/MonitorPointPlotter/src
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "SPLPlotter;exit;"
+cp /home/sal/Desktop/MATLAB/SPLPlotter/MonitorPointPlotter/FiguresOutput/* $input2/post-proc
+echo Near-Field Data Plots Created
+
+#Saving Plots of Far-Field Data
+echo Creating Far-Field Waterfall Plot
+cd /home/sal/Desktop/MATLAB/SPLPlotter/FWHPlotter/src
+cp $input1/sol/fwh_spl_ob* /home/sal/Desktop/MATLAB/SPLPlotter/FWHPlotter/input1
+cp $input2/sol/fwh_spl_ob* /home/sal/Desktop/MATLAB/SPLPlotter/FWHPlotter/input2
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "FWHPlotter_waterfall;exit;"
+
+echo Creating Individual Far-Field Plots
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "FWHPlotter_90;exit;"
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "FWHPlotter_120;exit;"
+"/home/sal/softwares/MATLAB/bin/matlab" -nodisplay -nosplash -nodesktop -r "FWHPlotter_150;exit;"
+cp /home/sal/Desktop/MATLAB/SPLPlotter/FWHPlotter/FiguresOutput/* $input2/post-proc/
+echo Far-Field Plots Created
+
+
