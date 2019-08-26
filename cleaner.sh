9772ca970ff9ffd020861a9260d68e784629d4f5
skr-code <salmanr96@gmail.com>
2019-08-26 17:35:08 -0500

    first working copy

---
+++ cleaner.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+rm -r  /home/sal/Desktop/CDNozzle1.47/ICDSV2/M2/p0-copy/post-proc
+
+rm /home/sal/Desktop/MATLAB/DataRakePlotter/External/input2/*
+rm /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/input2/*
+
+rm /home/sal/Desktop/MATLAB/DataRakePlotter/External/FiguresOutput/*
+rm /home/sal/Desktop/MATLAB/DataRakePlotter/Internal/FiguresOutput/*
+
+rm /home/sal/Desktop/MATLAB/MonotonicCleaner/monitor_data/*
+rm /home/sal/Desktop/MATLAB/MonotonicCleaner/extract_physical_data/*
+
+rm /home/sal/Desktop/MATLAB/SPLPlotter/FWHPlotter/FiguresOutput/*
+rm /home/sal/Desktop/MATLAB/SPLPlotter/MonitorPointPlotter/FiguresOutput/*
+
+rm /home/sal/Desktop/MATLAB/SPLPlotter/MonitorPointPlotter/input2/*
+rm /home/sal/Desktop/MATLAB/SPLPlotter/FWHPlotter/input2/*
